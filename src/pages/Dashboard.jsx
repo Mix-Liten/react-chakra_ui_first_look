04aeb36c0c7d11f6a765454eaecd5ff59b5e67ba
Mix-Liten <mixliten@gmail.com>
2023-03-29 13:52:55 +0800

    feat: update Dashboard & use Card compnent

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,26 +1,60 @@
-import { Box, SimpleGrid, Heading, Text } from '@chakra-ui/react'
+import { EditIcon, ViewIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  SimpleGrid,
+  Heading,
+  Text,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Flex,
+  HStack,
+  Button,
+  Divider,
+} from '@chakra-ui/react'
+import { useLoaderData } from 'react-router-dom'
 
 export default function Dashboard() {
+  const tasks = useLoaderData()
+
   return (
-    <SimpleGrid as="section" columns={4} spacing={10} minChildWidth={250} p="10px">
-      <Box bg="white" h="200px" border="1px solid">
-        <Text color={{ base: 'red', md: 'blue', lg: 'orange' }} fontSize={{ base: '24px', md: '40px', lg: '56px' }}>
-          Hello
-        </Text>
-      </Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
+    <SimpleGrid as="section" spacing={10} minChildWidth={300}>
+      {tasks &&
+        tasks.map(task => (
+          <Card key={task.id} borderTop="8px" borderColor="purple.400" bg="white">
+            <CardHeader>
+              <Flex gap={5}>
+                <Box w="50px" h="50px">
+                  <Text>AV</Text>
+                </Box>
+                <Box>
+                  <Heading as="h3" size="sm">
+                    {task.title}
+                  </Heading>
+                  <Text>by {task.author}</Text>
+                </Box>
+              </Flex>
+            </CardHeader>
 
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
+            <CardBody color="gray.500">
+              <Text>{task.description}</Text>
+            </CardBody>
 
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
+            <Divider borderColor="gray.200" />
+
+            <CardFooter>
+              <HStack>
+                <Button variant="ghost" leftIcon={<ViewIcon />}>
+                  Watch
+                </Button>
+                <Button variant="ghost" leftIcon={<EditIcon />}>
+                  Comment
+                </Button>
+              </HStack>
+            </CardFooter>
+          </Card>
+        ))}
     </SimpleGrid>
   )
 }
