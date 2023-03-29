48ecb6fdf3b0cfd6f049139bbbbd801036049075
Mix-Liten <mixliten@gmail.com>
2023-03-29 11:06:47 +0800

    feat: use Responsive Styles

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,11 @@ import { Box, SimpleGrid, Heading, Text } from '@chakra-ui/react'
 export default function Dashboard() {
   return (
     <SimpleGrid as="section" columns={4} spacing={10} minChildWidth={250} p="10px">
-      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid">
+        <Text color={{ base: 'red', md: 'blue', lg: 'orange' }} fontSize={{ base: '24px', md: '40px', lg: '56px' }}>
+          Hello
+        </Text>
+      </Box>
       <Box bg="white" h="200px" border="1px solid"></Box>
       <Box bg="white" h="200px" border="1px solid"></Box>
       <Box bg="white" h="200px" border="1px solid"></Box>
