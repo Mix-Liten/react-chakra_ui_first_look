61335c88d18c037b8ba95d19d4075ba9ba9b5601
Mix-Liten <mixliten@gmail.com>
2023-03-29 10:20:16 +0800

    feat: use Flex Layouts & add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Box, Button, Flex, Heading, HStack, Spacer, Text } from '@chakra-ui/react'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <Flex as="nav" p="10px" alignItems="center" gap="10px">
+      <Heading as="h1">Brief Tasks</Heading>
+      <Spacer />
+
+      <HStack spacing="20px">
+        <Box bg="gray.200" p="10px">
+          M
+        </Box>
+        <Text>fake@mail.com</Text>
+        <Button colorScheme="purple">Logout</Button>
+      </HStack>
+    </Flex>
+  )
+}
